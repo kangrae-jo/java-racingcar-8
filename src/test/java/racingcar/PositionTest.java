4d2aa39878b3f6786ad9c0ee8bfe7fd8b6d8e57b
kangrae <kangrae0501@gmail.com>
2025-10-22 13:34:01 +0900

    test: 자동차 위치 테스트 추가
    - 자동차 위치를 나타내는 Position은 0부터 시작한다.
    - Position은 move메서드의 인자만큼 position을 이동시킬 수 있다.

---
+++ src/test/java/racingcar/PositionTest.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PositionTest {
+
+    private static final int STEP = 1;
+
+    @Test
+    void 위치는_step만큼_움직인다() {
+        Position position = new Position();
+
+        int prev = 0;
+        int next = position.move(STEP);
+
+        assertThat(next).isEqualTo(prev + STEP);
+    }
+
+}
