3a5a4a677a55e589cfbfa259b252cc61ad300569
kangrae <kangrae0501@gmail.com>
2025-10-22 22:16:43 +0900

    fix: Round 생성자 파라미터 수정
    - String으로 받아서 내부에서 검증하도록 변경
    - 외부에서 parseInt() 하면 오류를 컨트롤할 수 없기 때문
    - 변경에 따른 테스트코드 일부 수정

--- src/main/java/racingcar/domain/round/Round.java
+++ src/main/java/racingcar/domain/round/Round.java
@@ -5,8 +5,9 @@ public class Round {
     private final int totalRound;
     private int currentRound;
 
-    public Round(int totalRound) {
-        this.totalRound = totalRound;
+    public Round(String totalRound) {
+        validateTotalRound(totalRound);
+        this.totalRound = Integer.parseInt(totalRound);
         this.currentRound = 0;
     }
 
@@ -19,6 +20,17 @@ public class Round {
             throw new IllegalArgumentException("[ERROR] 진행할 라운드가 없습니다.");
         }
         return currentRound += 1;
+    }
+
+    private void validateTotalRound(String totalRound) {
+        try {
+            int round = Integer.parseInt(totalRound);
+            if (round <= 0) {
+                throw new IllegalArgumentException("[ERROR] 라운드는 1 이상이어야 합니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 라운드는 숫자만 입력 가능합니다.");
+        }
     }
 
 }
--- src/test/java/racingcar/RoundTest.java
+++ src/test/java/racingcar/RoundTest.java
@@ -7,7 +7,7 @@ import racingcar.domain.round.Round;
 
 class RoundTest {
 
-    private static final int TOTAL_ROUND = 10;
+    private static final String TOTAL_ROUND = "10";
 
     @Test
     void 시도할_횟수_만큼만_라운드를_반복한다() {
@@ -19,7 +19,7 @@ class RoundTest {
             times++;
         }
 
-        assertThat(times).isEqualTo(TOTAL_ROUND);
+        assertThat(times).isEqualTo(Integer.parseInt(TOTAL_ROUND));
     }
 
 }
