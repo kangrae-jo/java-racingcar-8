432ef3d8383dfdcd414f4296c008b27cb879d07a
kangrae <kangrae0501@gmail.com>
2025-10-22 11:55:34 +0900

    feat: 자동차 이름 클래스 정의
    - 자동차 이름으로 앞뒤 공백은 trim처리 된다.
    - 자동차 이름은 1자 이상 5자 이하이다.

---
+++ src/main/java/racingcar/Name.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class Name {
+
+    private static final int NAME_LENGTH_LIMIT = 5;
+    private final String name;
+
+    public Name(String name) {
+        name = name.trim();
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.isEmpty() || name.length() > NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하입니다.");
+        }
+    }
+
+}
