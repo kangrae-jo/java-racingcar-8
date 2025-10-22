c576410e9732794c09b8db6abd621b5ab502b907
kangrae <kangrae0501@gmail.com>
2025-10-22 15:51:24 +0900

    test: 랜덤 수 테스트 추가
    - 랜덤한 수 하나의 객체로 생성되어 새로고침 가능
    - 랜덤한 수는 임계값을 매개변수로 받아 자동차가 움직이는 것이 가능한지 판단

---
+++ src/test/java/racingcar/RandomNumberTest.java
@@ -0,0 +1,47 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static racingcar.ApplicationTest.MOVING_FORWARD;
+import static racingcar.ApplicationTest.STOP;
+
+import org.junit.jupiter.api.Test;
+
+class RandomNumberTest {
+
+    @Test
+    void 랜덤수가_임계값_이상이면_전진한다() {
+        assertRandomNumberInRangeTest(() -> {
+                    RandomNumber randomNumber = new RandomNumber();
+                    assertThat(randomNumber.canMove(4)).isTrue();
+                }, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 랜덤수가_임계값_미만이면_전진하지_않는다() {
+
+        assertRandomNumberInRangeTest(() -> {
+                    RandomNumber randomNumber = new RandomNumber();
+                    assertThat(randomNumber.canMove(4)).isFalse();
+                }, STOP
+        );
+    }
+
+    @Test
+    void 랜덤수는_새로고침_할_수_있다() {
+        RandomNumber randomNumber = new RandomNumber();
+
+        assertRandomNumberInRangeTest(() -> {
+                    randomNumber.refresh();
+                    assertThat(randomNumber.canMove(4)).isTrue();
+                }, MOVING_FORWARD
+        );
+        assertRandomNumberInRangeTest(() -> {
+                    randomNumber.refresh();
+                    assertThat(randomNumber.canMove(4)).isFalse();
+                }, STOP
+        );
+    }
+
+}
