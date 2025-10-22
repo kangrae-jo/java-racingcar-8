6eaf277b59a37c9127a15b89fecd593550e634e6
kangrae <kangrae0501@gmail.com>
2025-10-22 14:39:26 +0900

    refactor: 상수 이름 변경 및 EOL 추가
    - Name의 이름 길이를 나타내는 상수의 의미가 중첩되지 않도록 NAME_LENGTH_LIMIT에서 NAME 제거
    - EOL 추가

--- src/main/java/racingcar/car/Name.java
+++ src/main/java/racingcar/car/Name.java
@@ -2,7 +2,7 @@ package racingcar.car;
 
 public class Name {
 
-    private static final int NAME_LENGTH_LIMIT = 5;
+    private static final int LENGTH_LIMIT = 5;
     private final String name;
 
     public Name(String name) {
@@ -12,7 +12,7 @@ public class Name {
     }
 
     private void validateName(String name) {
-        if (name.isEmpty() || name.length() > NAME_LENGTH_LIMIT) {
+        if (name.isEmpty() || name.length() > LENGTH_LIMIT) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하입니다.");
         }
     }
