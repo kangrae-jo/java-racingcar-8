44cb4d80d11ac498bf613a5355c1961f95e05376
kangrae <kangrae0501@gmail.com>
2025-10-25 19:42:10 +0900

    feat: 사용자 입력을 유도하는 프롬프트 출력 기능 추가
    - OutputView에 static 메서드를 추가해 콘솔에 입력 프롬프트를 출력하도록 구현
    - MVC 패턴에서 View 책임을 분리하되, 인자 증가를 피하기 위해 static 방식 채택
    - 도메인 중심 구조에서 View 호출의 단순성과 명확성을 유지

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,10 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String readCarNames() {
+        OutputView.printCarNamePrompt();
         return Console.readLine();
     }
 
     public static String readTotalRound() {
+        OutputView.printRoundPrompt();
         return Console.readLine();
     }
 
