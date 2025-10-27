ac0e1ee2449dc18d95e08cca806b1f97e5fbc03d
kangrae <kangrae0501@gmail.com>
2025-10-27 16:23:01 +0900

    feat: OutputView 생성자를 통한 인스턴스 생성을 막는 기능 구현
    - 생성자를 private으로 설정하여 불필요한 인스턴스화 방지

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,9 @@ import racingcar.domain.car.dto.WinnerDto;
 
 public class OutputView {
 
+    private OutputView() {
+    }
+
     public static void printCarNamePrompt() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
